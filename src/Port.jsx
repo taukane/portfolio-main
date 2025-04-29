0b94ae9b28769b48204d27c6976454c076acac5d
Taukane <taukane@yahoo.com.br>
2025-04-29 10:10:34 -0300

    Fixes Animações e Menu

--- src/Port.jsx
+++ src/Port.jsx
@@ -56,11 +56,11 @@ return (
 <>
 <span className="scroller"></span>
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid d-flex justify-content-end">
+    <div className="container-fluid">
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-center d-flex" id="navbarNav">
+        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
         <ul className="navbar-nav">
             <li className="nav-item">
             <NavLink className="nav-link" href="#" to="/">
